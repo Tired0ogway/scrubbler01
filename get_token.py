46c9f54c987ece76b29022430e55945f65f94c86
Tired.0ogway <87137686+Tired0ogway@users.noreply.github.com>
2025-05-09 18:59:48 +0530

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -2,9 +2,9 @@ import hashlib
 
 import requests
 
-API_KEY = "6267962a5e22a210191c11109c814dd3"
-SECRET = "aad952b66ed4651379314b01173bc395"
-TOKEN = "HWINaMTX5rdlxdqQj6hHygsb7C65kFsP"
+API_KEY = ""
+SECRET = ""
+TOKEN = ""
 
 # Generate API signature
 api_sig_str = f"api_key{API_KEY}methodauth.getSessiontoken{TOKEN}{SECRET}"
